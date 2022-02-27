5a1f2fb4046f4df3bd74747e3a8f84900a02ca79
rafaelkenedy <k.rafaelalves@gmail.com>
2022-02-26 23:51:23 -0300

    axios added

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { Text, View, Button } from 'react-native'
+import api from './src/services/api'
 
 const App = () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos/201'
+  // const url = 'https://jsonplaceholder.typicode.com/todos/201'
 
   // useEffect(() => {
   //   fetch(url)
@@ -11,20 +12,46 @@ const App = () => {
   //     .catch(error => console.warn(error))
   // }, [])
 
-  const submit = () => {
-    const data = {
+  // const submit = () => {
+  //   const data = {
+  //     title: 'Todo test',
+  //     body: 'test',
+  //     userId: 1,
+  //   };
+  //   const params = {
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //   };
+  //   fetch(url, params).then(response => response.json()).then(json => console.log(json)).catch(error => console.warn(error))
+  // }
+
+  useEffect(() => {
+    async function handleRequest() {
+      try {
+        const { data } = await api.get('/todos')
+        console.log(data)
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+    handleRequest()
+  }, [])
+
+  const submit = async () => {
+    const payload = {
       title: 'Todo test',
       body: 'test',
       userId: 1,
-    };
-    const params = {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    };
-    fetch(url, params).then(response => response.json()).then(json => console.log(json)).catch(error => console.warn(error))
+    }
+    try {
+      const { data } = await api.post('/todos', payload)
+      console.log(data)
+    } catch (error) {
+      console.warn(error)
+    }
   }
 
   return (
